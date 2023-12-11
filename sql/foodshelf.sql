5bf7f47f1dad65e1f8d70baaa100f9f6199ecae1
tomoyasu <tomoyasu0504.9115@gmail.com>
2023-12-11 18:50:20 +0900

    初期プロジェクト構築とDocker環境構築。

---
+++ sql/foodshelf.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS movies;
+
+CREATE TABLE foodshelf (
+  id int unsigned AUTO_INCREMENT,
+  food_name VARCHAR(20) NOT NULL,
+  expiration_date date NOT NULL,
+  sending_times int unsigned NOT NULL,
+  PRIMARY KEY(id)
+ );
+
+ INSERT INTO notifications (food_name, expiration_date, sending_times) VALUES ("鶏肉",　"2023-12-01", 3);
+INSERT INTO notifications (food_name, expiration_date, sending_times) VALUES ("牛肉",　"2023-12-11", 2);
+INSERT INTO notifications (food_name, expiration_date, sending_times) VALUES ("卵",　"2023-12-21", 1);
+INSERT INTO notifications (food_name, expiration_date, sending_times) VALUES ("玉ねぎ",　"2023-01-01", 2);
+INSERT INTO notifications (food_name, expiration_date, sending_times) VALUES ("にんじん",　"2023-01-11", 2);
+INSERT INTO notifications (food_name, expiration_date, sending_times) VALUES ("じゃがいも",　"2023-01-21", 3);
