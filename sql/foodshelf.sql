fc96a46b387de03b0f05e7b636a85faf437040d8
tomoyasu <tomoyasu0504.9115@gmail.com>
2023-12-19 08:33:44 +0900

    sqlを一部修正

--- sql/foodshelf.sql
+++ sql/foodshelf.sql
@@ -8,9 +8,11 @@ CREATE TABLE foodshelf (
   PRIMARY KEY(id)
  );
 
-INSERT INTO notifications (food_name, expiration_date, sending_times) VALUES ("鶏肉",　"2023-12-01", 3);
-INSERT INTO notifications (food_name, expiration_date, sending_times) VALUES ("牛肉",　"2023-12-11", 2);
-INSERT INTO notifications (food_name, expiration_date, sending_times) VALUES ("卵",　"2023-12-21", 1);
-INSERT INTO notifications (food_name, expiration_date, sending_times) VALUES ("玉ねぎ",　"2023-01-01", 2);
-INSERT INTO notifications (food_name, expiration_date, sending_times) VALUES ("にんじん",　"2023-01-11", 2);
-INSERT INTO notifications (food_name, expiration_date, sending_times) VALUES ("じゃがいも",　"2023-01-21", 3);
+INSERT INTO foodshelf (food_name, expiration_date, sending_times)
+VALUES
+  ("鶏肉", "2023-12-01", 3),
+  ("牛肉", "2023-12-11", 2),
+  ("卵", "2023-12-21", 1),
+  ("玉ねぎ", "2023-01-01", 2),
+  ("にんじん", "2023-01-11", 2),
+  ("じゃがいも", "2023-01-21", 3);
